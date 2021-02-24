d80fc98d7be968855a1e37f854e0f3a3fdc42150
Carla Machado Paiano <machadogcarla@gmail.com>
2021-02-23 21:27:25 -0300

    Botão de pesquisar por usuário e lista ordenada por estrelas

--- src/App.vue
+++ src/App.vue
@@ -1,19 +1,19 @@
 <template>
-
   <div>
     <div class="container">
+            
       <div class="search card card-body">
           <p class="titulo1">GitHub <span class="titulo2">Search</span></p>
-        <form>
-          <div class="form-group row">
-            <input class="col-sm-11 col-form-label" @keyup="getUser" required/>
-            <div class="col-sm-1">
-              <button class="btn btn-dark btn-block"> <v-icon class="material-icons"> search </v-icon></button>
-              <!--<button >a</button>-->
-            </div>
-          </div>
-          
-        </form>
+
+            <form @submit="getUser">
+                <div class="form-group row">
+                  <input class="col-sm-11 col-form-label" type="text"  id="search" required/>
+                  <div class="col-sm-1">
+                    <button class="btn btn-dark btn-block"> <v-icon class="material-icons"> search </v-icon></button>
+                  </div>
+              </div>
+            </form>
+            
       </div>
 
       <div
@@ -21,59 +21,76 @@
         v-if="user.length !== 0"
       >
         <div class="col-md-4">
-          <Profile :user="user" />
+          <Profile :user="user" />   
         </div>
         <div class="col-md-8">
           <Repos
-            v-for="repo in repos"
+            v-for="repo in orderedRepos"
             :repo="repo"
             :key="repo"
+             
           />
         </div>
       </div>
     </div>
   </div>
+ 
 </template>
 
 <script>
+
+import _ from 'lodash';
+import axios from 'axios';
 import Profile from "./components/Profile.vue";
 import Repos from "./components/Repos.vue";
-import axios from "axios";
 
 export default {
   name: "app",
+    
   data() {
-    return {
+
+      return {
       github: {
         url: "https://api.github.com/users",
-        client_id: "044ecbe0f9cd24459823",
-        client_secret: "440f7e976118b387fca6d14d9dbe9b742bf03d05",
-        count: 10,
-        sort: "created: asc"
+        //client_id: "3101ae14397bfa4010a2d",
+        //client_secret: "46fa1f407bc140a181b2f013bab033b954e23a18",
+        count: "10",
       },
       user: [],
       repos: []
     };
   },
-  components: {
+    components: {
     Profile,
     Repos
   },
+   computed: {
+  orderedRepos: function () {
+    return _.orderBy(this.repos, 'stargazers_count', 'desc');
+
+  }
+  },
+
   methods: {
-    getUser(e) {
-      const user = e.target.value;
-      const { url, client_id, client_secret, count, sort } = this.github;
-      axios
-        .get(
-          `${url}/${user}?client_id=${client_id}&client_secret=${client_secret}`
+   getUser: function (e) {   
+     e.preventDefault();    
+    
+    var search = document.getElementById("search").value;
+    
+    const { url, count} = this.github;
+      axios.get(
+          `${url}/${search}`
         )
         .then(({ data }) => (this.user = data));
-      axios
+
+        axios
         .get(
-          `${url}/${user}/repos?per_page=${count}&sort=${sort}&client_id=${client_id}&client_secret=${client_secret}`
+          `${url}/${search}/repos?per_page=${count}`
         )
         .then(({ data }) => (this.repos = data));
-    }
+      
   }
+  }
+ 
 };
 </script>
--- src/components/Profile.vue
+++ src/components/Profile.vue
@@ -31,7 +31,7 @@
 
 <script>
     export default {
-    props: ["user"],
-    };
+    props: ["user", "repo"],
+};
 
 </script>
