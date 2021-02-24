6fbb2ee76475135c8494b4d504d143313af9ef60
Carla Machado Paiano <machadogcarla@gmail.com>
2021-02-24 18:57:12 -0300

    atualizacao server.js

--- server.js
+++ server.js
@@ -1,10 +1,12 @@
-var express = require('express')
-var path = require('path')
-var serveStatic = require('serve-static')
+const express = require("express");
+const port = process.env.PORT || 5000;
+const app = express();
 
-var app = express()
-app.use(serveStatic(path.join(__dirname, 'dist')))
+app.use(express.static(__dirname + "/dist"));
+app.get(/.*/, function(req,res){
+    res.sendFile(__dirname + "/dist/index.html");
+});
 
-var port = process.env.PORT || 3000
-app.listen(port)
-console.log('server started' + port)
+app.listen(port);
+console.log("Server started...");
+
